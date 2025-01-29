ca2f07bb9203f3c61227e1be5e0ca655e8d11603
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-29 12:07:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -324,7 +324,7 @@ def main():
 
         #---------------------------------------------------------------------------------------------------------------------
         #Таблица с постами Лидеры и оутсайдеры
-        st.pyplot(create_table_top5(posts, post_view, subs, gr_pvr,  selected_channel, bgcolor, word_color, cmap_colors), bbox_inches='tight')
+        st.pyplot(create_table_top5(posts, post_view, subs, gr_pvr,  selected_channel, bgcolor, word_color, cmap_colors))
 
     #---------------------------------------------------------------------------------------------------------------------
     # Таблица - динамика просмотров
