a824cb537a5c4c2e4b3033e3bebbc0d8ec71cf79
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-31 20:28:10 +0500

    Update fig_table_top5.py

--- graph_functions/fig_table_top5.py
+++ graph_functions/fig_table_top5.py
@@ -276,7 +276,7 @@ def create_table_top5(posts, post_view, subs, gr_pvr,  channel, bgcolor='#FFA500
                         
                     circle_size = int(35 + normalized_value * 30)
                     circle_color = plt.cm.autumn(normalized_value)[:3]
-                    if col != 'Индекс':
+                    if col != 'Индекс вовлеченности':
                         value = int(value)
                     circle_color_hex = "#{:02x}{:02x}{:02x}".format(int(circle_color[0]*255), int(circle_color[1]*255), int(circle_color[2]*255))
                     html += f"<td><div class='circle' style='width: {circle_size}px;height: {circle_size}px;line-height: {circle_size}px;background-color: {circle_color_hex};color: #333; font-size: {font_size}px;'>{value}</div></td>"
