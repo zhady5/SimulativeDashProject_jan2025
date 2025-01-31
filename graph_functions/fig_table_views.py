8d2b7e9eeb18db84124287a2626939e38eb15617
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-31 22:00:59 +0500

    Update fig_table_views.py

--- graph_functions/fig_table_views.py
+++ graph_functions/fig_table_views.py
@@ -98,8 +98,10 @@ def styled_df(df, dark_color = '#8B0000', clr='#006a4e'):
     
             log_values = [log_scale(v) for v in s]
             max_log_value = max(log_values) if log_values else 1  # Avoid division by zero
-            return ['background: linear-gradient(90deg, {} {:.1f}%, white {:.1f}%)'.format(clr, v, 100-v) 
-                    for v in (log_values / max_log_value * 100)]
+            
+            # Используем генератор списков для выполнения деления для каждого элемента
+            return ['background: linear-gradient(90deg, {} {:.1f}%, white {:.1f}%)'.format(clr, (v / max_log_value * 100), (100 - v / max_log_value * 100)) 
+                    for v in log_values]
         return [''] * len(s)
     # Применение функции стилей ко всем ячейкам DataFrame
     styled_df = df.style.map(style_contains).apply(add_bar, axis=0)
