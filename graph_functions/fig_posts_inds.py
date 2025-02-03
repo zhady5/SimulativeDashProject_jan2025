624fca413d2f3fc1a2401ca29ee0fbeb1d06b8a5
Mika <42176090+zhady5@users.noreply.github.com>
2025-02-03 20:27:39 +0500

    Update fig_posts_inds.py

--- graph_functions/fig_posts_inds.py
+++ graph_functions/fig_posts_inds.py
@@ -72,7 +72,7 @@ def create_fig_posts_inds(posts, selected_channel, date_range, bgcolor='#ffb347'
                 value=current,
                 title={"text": f"<span style='font-size:0.85em;color:{word_color}'>Публикаций за {period_names[period[0]]}<br><span style='font-size:0.8em;color:gray'>Пред. знач.: {round(previous)}</span>"},
                 mode="number+delta",
-                number={'font': {'size': 68}, 'color': f'{graph_color}'},  # Задаем размер шрифта для текущего значения
+                number={'font': {'size': 68}},  # Задаем размер шрифта для текущего значения
                 delta={'reference': previous, 'relative': True, "valueformat": ".2%", 'font': {'size': 18}},
             ), row=i + 1, col=2
         )
